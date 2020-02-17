1a583e5528cccf252468f95506d1ef8fadd5fe43
André Ferreira <mrawesomeferreira@gmail.com>
2020-02-17 18:44:35 +0000

    on iteration 2

--- models/user.js
+++ models/user.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // User model goes here
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('User', schema);
---
+++ routes/authentication.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const { Router } = require('express');
+const router = new Router();
+
+const bcryptjs = require('bcryptjs');
+
+const User = require('../models/user');
+
+// --------------Sign In
+
+router.get('/sign-in', (req, res, next) => {
+  res.render('authentication/sign-in');
+});
+
+router.post('/sign-in', (req, res, next) => {
+  const { name, password } = req.body;
+
+  let user;
+
+  User.findOne({ name })
+    .then(document => {
+      if (!document) {
+        next(new Error('USER_NOT_FOUND'));
+      } else {
+        user = document;
+        return bcryptjs.compare(password, document.passwordHash);
+      }
+    })
+    .then(match => {
+      if (match) {
+        req.session.userId = user._id;
+        res.redirect('/');
+      } else {
+        next(new Error('USER_PASSWORD_WRONG'));
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+// ---------------------------- Sign Up
+
+router.get('/sign-up', (req, res) => {
+  res.render('authentication/sign-up');
+});
+
+router.post('/sign-up', (req, res, next) => {
+  const { password, name } = req.body;
+
+  bcryptjs
+    .hash(password, 10)
+    .then(hashPlusSalt => {
+      return User.create({
+        name,
+        passwordHash: hashPlusSalt
+      });
+    })
+    .then(user => {
+      req.session.userId = user._id;
+      res.redirect('/');
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+/* router.post('/sign-out', (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+ */
+module.exports = router;
